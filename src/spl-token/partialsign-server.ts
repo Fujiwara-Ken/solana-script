3ff18215d40b4b51acab4407e265e57f596e47ea
Fujiwara-Ken <fujiwara.k@impl.co.jp>
2022-07-27 16:26:13 +0900

    partialsign server fix

--- src/spl-token/partialsign-server.ts
+++ src/spl-token/partialsign-server.ts
@@ -5,10 +5,11 @@
 import assert from "assert";
 import { PartialSignInstruction } from "@solana-suite/shared";
 
+// clientから送信されたhexデータを受け取り移転完了させる関数
 export const submitSplHex = async (hex: string, feePayer: string) => {
-  const obj2 = new PartialSignInstruction(hex);
-  const res2 = await obj2.submit(feePayer.toKeypair());
-  res2.match(
+  const obj = new PartialSignInstruction(hex);
+  const res = await obj.submit(feePayer.toKeypair());
+  res.match(
     (ok) => console.log("# tx signature: ", ok),
     (err) => assert.fail(err.message)
   );
